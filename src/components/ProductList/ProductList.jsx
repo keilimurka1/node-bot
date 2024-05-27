eb66d25679c1a798657b243e3993817c39a03e8b
John Doe <adventurejojo28@gmail.com>
2024-05-27 06:57:49 +0500

    Updated submodule configura744tion

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -5,14 +5,14 @@ import {useTelegram} from "../../hooks/useTelegram";
 import {useCallback, useEffect} from "react";
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '1', title: 'Laser', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '2', title: 'Phaser', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '3', title: 'Battle axe 2', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '4', title: 'Jinjerblade', price: 122, description: 'Зеленого цвета, теплая'},
+    {id: '5', title: 'Boneblade', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '6', title: 'Peppermint', price: 600, description: 'Зеленого цвета, теплая'},
+    {id: '7', title: 'Euphoria', price: 5500, description: 'Синего цвета, прямые'},
+    {id: '8', title: 'Idol', price: 12000, description: 'Зеленого цвета, теплая'},
 ]
 
 const getTotalPrice = (items = []) => {
